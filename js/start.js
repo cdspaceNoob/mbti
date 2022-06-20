516ed12852dd523c3c5763eec6bb9042aa033fae
voyager <cdspacenoob@gmail.com>
2022-06-20 19:16:11 +0900

    createElement btn

--- js/start.js
+++ js/start.js
@@ -1,6 +1,22 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 
+function addAnswer(answerText) {
+  var a = document.querySelector("#answerBox");
+  var answer = document.createElement("button");
+  // answer가 a에 속할 수 있도록
+  a.appendChild(answer);
+  answer.innerHTML = answerText;
+}
+
+function goNext(qIdx) {
+  var q = document.querySelector("#qBox");
+  q.innerHTML = qnaList[qIdx].q;
+  for (let i in qnaList[qIdx].a) {
+    addAnswer(qnaList[qIdx].a[i].answer);
+  }
+}
+
 function begin() {
   main.style.WebkitAnimation = "fadeOut 1s";
   main.style.animaion = "fadeOut 1s";
@@ -11,5 +27,7 @@ function begin() {
       main.style.display = "none";
       qna.style.display = "block";
     }, 450);
+    let qIdx = 0;
+    goNext(qIdx);
   }, 450);
 }
